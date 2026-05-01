37103e3529cc295c751ef4877c1fd0ae05facbe6
Jheron Dylan Conde Martinez <jheronconde032@gmail.com>
2026-05-01 12:45:05 -0400

    feat(engine): add population counter

--- proyecto/engine.js
+++ proyecto/engine.js
@@ -3,6 +3,9 @@ const Engine = (() => {
   const create = (cols, rows) =>
     Array.from({ length: rows }, () => Array(cols).fill(0));
 
-  return { create };
+  const population = (grid) =>
+    grid.reduce((s, row) => s + row.reduce((r, c) => r + c, 0), 0);
+
+  return { create, population };
 
 })();
