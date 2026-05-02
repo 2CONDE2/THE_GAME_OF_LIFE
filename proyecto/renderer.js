aa478052ebbf72abe86adfc4ca9afc68ce24c6f5
Gabriel Bazualdo <bazualdogabriel@gmail.com>
2026-05-02 19:50:33 -0400

    refactor(renderer): extract style and dimension constants

--- proyecto/renderer.js
+++ proyecto/renderer.js
@@ -1,4 +1,9 @@
 const Renderer = (() => {
+  const CELL = 13;
+  const GAP  = 1;
+  const C_ALIVE = '#dde8ff';
+  const C_DEAD  = '#111118';
+
   let canvas, ctx;
 
   const init = (el) => {
@@ -14,3 +19,4 @@ const Renderer = (() => {
 
   return { init };
 })();
+
